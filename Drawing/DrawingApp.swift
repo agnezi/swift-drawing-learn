e81f58a15e47b820cce22dcb4485e33851cba575
Jonas Agnezi <jonas.agnezi@gmail.com>
2022-10-05 19:56:56 -0300

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Jonas Agnezi on 05/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
